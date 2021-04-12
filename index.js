359fa5dcaa3ccbb38fb71441ea099e7a94266aa1
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2021-04-12 15:14:57 +0200

    .

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function handleRequest(request) {
     r.get('.*/bar', () => new Response('responding for /bar'))
     r.get('.*/foo', request => handler(request))
     r.post('.*/foo.*', request => handler(request))
-    r.get('/demos/router/foo', request => fetch(request)) // return the response from the origin
+    //r.get('/demos/router/foo', request => fetch(request)) // return the response from the origin
 
     r.get('/', () => new Response('Hello worker!')) // return a default message for the root route
 
